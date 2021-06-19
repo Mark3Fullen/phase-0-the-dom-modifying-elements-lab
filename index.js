65fd52a0b6aa61d4f4d9757255e1a6f33087a3d5
Mark <mark3fullen@gmail.com>
2021-06-19 12:32:00 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+main.remove(main);
+
+var newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = '<h1>Neilson is the champion</h1>';
